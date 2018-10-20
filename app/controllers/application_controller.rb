0ab9b5bd348b7a1ecf5e199797f6dc2aa04f14e6
Damian Michniak <damian.michniak29@gmail.com>
2018-10-20 16:12:03 +0000

    step 1 rails setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def add
+    @answer = params[:x].to_i + params[:y].to_i
+    
+  end
 end
